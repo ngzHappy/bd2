b65c67f2c56750bc1f52d1aa8c89f20bea12a498
ngzHappy <819869472@qq.com>
2016-09-28 20:53:50 +0800

    +visual build bat

---
+++ visual_studio_build/build.bat
@@ -0,0 +1,15 @@
+PATH=D:\x\QT_VS2015_57\qt-everywhere-opensource-src-5.7.0-beta\qtbase\bin;%PATH%;
+qmake -r -tp vc "../"
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ visual_studio_build/readme.txt
@@ -0,0 +1,3 @@
+PATH=D:\x\QT_VS2015_57\qt-everywhere-opensource-src-5.7.0-beta\qtbase\bin;%PATH%;
+change the path to your qmake.exe dir
+
