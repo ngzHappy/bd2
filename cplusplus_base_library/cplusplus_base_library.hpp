31d8e77d8130d006db27d002212eb06ac062d62e
ngzHappy <819869472@qq.com>
2016-09-28 21:15:26 +0800

    增加目录结构

--- cplusplus_base_library/cplusplus_base_library.hpp
+++ cplusplus_base_library/cplusplus_base_library.hpp
@@ -1,6 +1,16 @@
+﻿#ifndef _mc_CPLUSPLUS_BASELIBRARY__
+#define _mc_CPLUSPLUS_BASELIBRARY__
+
 #include "private_cplusplus_base_library/cplusplus_base_library_global.hpp"
 
+class CPLUSPLUS_BASE_LIBRARYSHARED_EXPORT cplusplus_base_library {
+public:
+    cplusplus_base_library();
+    virtual~cplusplus_base_library();
+};
 
+
+#endif
 
 
 
--- cplusplus_base_library/source_cplusplus_base_library/source_cpp_global.cpp
+++ cplusplus_base_library/source_cplusplus_base_library/source_cpp_global.cpp
@@ -1,6 +1,14 @@
 ﻿#include "../private_cplusplus_base_library/cplusplus_base_library_global.hpp"
+#include "../cplusplus_base_library.hpp"
+
+/*用于测试动态链接库*/
+cplusplus_base_library::cplusplus_base_library(){
+
+}
+
+cplusplus_base_library::~cplusplus_base_library(){
+
+}
 
 
-
-
-
+/*endl of the file*/
