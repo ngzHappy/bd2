05c3b64ef77feac0b6e2e4f8ba8a2b16501879e1
ngzHappy <819869472@qq.com>
2016-10-13 10:48:30 +0800

    +test

--- test/test_quicktest/BoostICL.cpp
+++ test/test_quicktest/BoostICL.cpp
@@ -1,6 +1,29 @@
 ﻿#include "BoostICL.hpp"
 
-BoostICL::BoostICL()
-{
+#include <iostream>
+#include <Qt/boost/icl/interval_set.hpp>
+
+BoostICL::BoostICL() {
+    using t=boost::icl::interval_set<double>;
+    using tt=t::interval_type;
+    t test;
+    test.insert(tt::closed(1,2));
+    test.insert(tt::closed(5,9));
+
+    {
+        auto x=test.equal_range(tt::closed(3,3.3));
+
+        for (auto i=x.first; i!=x.second; ++i) {
+            std::cout<<i->lower()<<" , "<<i->upper()<<std::endl;
+        }
+    }
+
+    {
+        auto x=test.equal_range(tt::closed(1.1,3.3));
+
+        for (auto i=x.first; i!=x.second; ++i) {
+            std::cout<<i->lower()<<" , "<<i->upper()<<std::endl;
+        }
+    }
 
 }
